de4c6476dcf741ca778c42a6da86848904cb50b5
Ethan Leifer <ethanleifer@gmail.com>
2023-04-14 11:07:11 -0400

    drop tables on startup fix

--- packages/indexer/indexer/main.py
+++ packages/indexer/indexer/main.py
@@ -1,13 +1,13 @@
 import asyncio
 import os
-from typing import Any, Awaitable, Callable, Coroutine
+from typing import Callable, Coroutine
 from aiohttp import ClientSession
 
 from asyncpg import Pool, create_pool
 from indexer.backfill import backfill
 
 from indexer.chain import get_chain_from_environment, CosmosChain
-from indexer.db import create_tables
+from indexer.db import create_tables, drop_tables
 from indexer.live import live
 
 
@@ -29,6 +29,9 @@ async def main():
         server_settings={"search_path": schema_name},
     ) as pool:
         async with pool.acquire() as conn:
+            DROP_TABLES_ON_STARTUP = os.getenv("DROP_TABLES_ON_STARTUP", "True").upper() == "TRUE"
+            if DROP_TABLES_ON_STARTUP:
+                await drop_tables(conn, schema_name)
             await create_tables(conn, schema_name)
 
         await asyncio.gather(run(pool, live), run(pool, backfill))
