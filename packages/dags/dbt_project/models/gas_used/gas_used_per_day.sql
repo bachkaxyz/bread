b64fdaf4083ef8d0586b799365bdc61c87f6b1ff
Ethan Leifer <ethanleifer@gmail.com>
2023-04-17 17:40:28 -0400

    sql formatting

--- packages/dags/dbt_project/models/gas_used/gas_used_per_day.sql
+++ packages/dags/dbt_project/models/gas_used/gas_used_per_day.sql
@@ -1,9 +1,7 @@
-{{ config(materialized='view') }}
+{{ config(materialized="view") }}
 
-select 
-    sum(gas_used),
-    date_trunc('day', timestamp) as "day" 
-from {{ source('indexer', 'txs') }}
+select sum(gas_used), date_trunc('day', timestamp) as "day"
+from {{ source("indexer", "txs") }}
 
 group by "day"
 order by "day"
--- packages/dags/dbt_project/models/txs/cum_txs_per_day.sql
+++ packages/dags/dbt_project/models/txs/cum_txs_per_day.sql
@@ -1,11 +1,14 @@
-{{ config(materialized='view') }}
+{{ config(materialized="view") }}
 
-with cum_txs_per_day as (
-    select 
-        "day", 
-        sum(tx_count) over (order by "day" asc rows between unbounded preceding and current row) as "tx_count"
-    from {{ ref('num_txs_per_day')}}
-) 
+with
+    cum_txs_per_day as (
+        select
+            "day",
+            sum(tx_count) over (
+                order by "day" asc rows between unbounded preceding and current row
+            ) as "tx_count"
+        from {{ ref("num_txs_per_day") }}
+    )
 
 select *
 from cum_txs_per_day
