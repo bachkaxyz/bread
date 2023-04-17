36d3bbe8bb0d7fdc68d06ca2a09f74ac7c168c1c
Ethan Leifer <ethanleifer@gmail.com>
2023-04-17 17:36:46 -0400

    finish prod setup

--- scripts/run_dagster.sh
+++ scripts/run_dagster.sh
@@ -7,11 +7,14 @@ if [ $ENVIRONMENT == ""]; then
   echo "please set ENVIRONMENT to either 'production' or 'development' in root .env";
   exit 0;
 elif [ $ENVIRONMENT == "production" ]; then
-    echo "production mode not setup yet";
+    echo "production mode";
+    cd ./packages/dags;
+    docker compose -f docker-compose.prod.yaml down;
+    docker compose -f docker-compose.prod.yaml up -d;
     exit 0;
 elif [ $ENVIRONMENT == "development" ]; then
     echo "development mode";
-    cd ./packages/jobs;
+    cd ./packages/dags;
     docker compose -f docker-compose.local.yaml down;
     DOCKER_BUILDKIT=0 docker compose -f docker-compose.local.yaml build;
     docker compose -f docker-compose.local.yaml up -d;
