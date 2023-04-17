d08441f6e68e293a7b5c17145278f27ccc2d4d72
Ethan Leifer <ethanleifer@gmail.com>
2023-04-17 12:41:25 -0400

    another change

--- scripts/run_indexer.sh
+++ scripts/run_indexer.sh
@@ -3,11 +3,14 @@ source scripts/_load_root_env_variables.sh;
 echo ENVIRONMENT: $ENVIRONMENT;
 echo BRANCH NAME: $GIT_BRANCH;
 
-if [ $ENVIRONMENT == "production" ]; then
+if [ $ENVIRONMENT == ""]; then
+  echo "please set ENVIRONMENT to either 'production' or 'development' in root .env";
+  exit 0;
+elif [ $ENVIRONMENT == "production" ]; then
     echo "production mode";
     cd ./packages/indexer;
-    docker compose -f docker-compose.yaml down;
-    docker compose -f docker-compose.yaml up -d;
+    sudo docker compose -f docker-compose.yaml down;
+    sudo docker compose -f docker-compose.yaml up -d;
 elif [ $ENVIRONMENT == "development" ]; then
     echo "development mode";
     cd ./packages/indexer;
