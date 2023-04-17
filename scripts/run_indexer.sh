bb676e3a0d096e5e8eed823b8220f9f04fef469d
Ethan Leifer <ethanleifer@gmail.com>
2023-04-17 13:45:57 -0400

    change

--- scripts/run_indexer.sh
+++ scripts/run_indexer.sh
@@ -13,7 +13,7 @@ if [ "$ENVIRONMENT" == ""]; then
 elif [ "$ENVIRONMENT" == "production" ]; then
     echo "production mode";
     cd ./packages/indexer;
-    export GIT_BRANCH=$GIT_BRANCH;
+    sudo export GIT_BRANCH=$GIT_BRANCH;
     sudo docker compose -f docker-compose.yaml down;
     sudo docker compose -f docker-compose.yaml up -d;
     
