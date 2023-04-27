ac6cc6d84bfa8be3fe0031bf8a997bf8cc1dbcf8
Ethan Leifer <ethanleifer@gmail.com>
2023-04-27 15:46:32 -0400

    remove old creds in script

--- scripts/run_indexer.sh
+++ scripts/run_indexer.sh
@@ -14,25 +14,25 @@ if [ "$GOOGLE_APPLICATION_CREDENTIALS" == "" ]; then
   exit 0;
 fi;
 
+cd ./packages/indexer;
+rm creds.json
+
 if [ "$ENVIRONMENT" == ""]; then
   echo "please set ENVIRONMENT to either 'production' or 'development' in root .env";
   exit 0;
 elif [ "$ENVIRONMENT" == "production" ]; then
     echo "production mode";
-    cd ./packages/indexer;
     docker compose -f docker-compose.yaml down;
     DOCKER_BUILDKIT=0 docker compose -f docker-compose.yaml build;
     docker compose -f docker-compose.yaml up -d;
     
 elif [ "$ENVIRONMENT" == "development" ]; then
     echo development mode;
-    cd ./packages/indexer;
     docker compose -f docker-compose.yaml -f docker-compose.local.yaml down;
     DOCKER_BUILDKIT=0 docker compose -f docker-compose.yaml -f docker-compose.local.yaml build;
     docker compose -f docker-compose.yaml -f docker-compose.local.yaml up -d;
 elif [ "$ENVIRONMENT" == "testing" ]; then
     echo testing mode;
-    cd ./packages/indexer;
     docker compose -f docker-compose.tests.yaml down --remove-orphans;
     DOCKER_BUILDKIT=0 docker compose -f docker-compose.tests.yaml build;
     docker compose -f docker-compose.tests.yaml up --abort-on-container-exit;
