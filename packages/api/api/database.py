e3a8b731fbaf32310acaa18acd6a75d44b904e94
Ethan Leifer <ethanleifer@gmail.com>
2023-04-27 20:56:15 -0400

    fix database stuff

--- packages/api/api/database.py
+++ packages/api/api/database.py
@@ -1,9 +1,23 @@
 import os
 import databases
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
 
 load_dotenv()
 
-database_url = os.getenv("DATABASE_URL")
+host = os.getenv("POSTGRES_HOST")
+port = os.getenv("POSTGRES_PORT")
+user = os.getenv("POSTGRES_USER")
+password = quote_plus(os.getenv("POSTGRES_PASSWORD", ""))
+database = os.getenv("POSTGRES_DB")
+
+DBT_SCHEMA = os.getenv("DBT_SCHEMA", "public")
+INDEXER_SCHEMA = os.getenv("INDEXER_SCHEMA", "public")
+DAGSTER_SCHEMA = os.getenv("DAGSTER_SCHEMA", "public")
+
+database_url = f"postgresql://{user}:{password}@{host}:{port}/{database}"
+
+if database_url is None:
+    raise Exception("POSTGRES_URI environment variable not set")
 database = databases.Database(database_url)
--- packages/api/api/routers/txs.py
+++ packages/api/api/routers/txs.py
@@ -1,6 +1,8 @@
 from api.database import database
 from fastapi import APIRouter
 
+from api.database import DBT_SCHEMA
+
 router = APIRouter(
     prefix="/txs", tags=["txs"], responses={404: {"description": "Not found"}}
 )
@@ -9,18 +11,9 @@ router = APIRouter(
 @router.get("/cumulative")
 async def get_cumulative_txs():
     return await database.fetch_all(
-        """
-        with num_txs_per_day as (
-            select count(1), date_trunc('day', timestamp) as d
-            from txs
-            group by d
-            order by d
-        ) 
-
-        select
-        d as "day",
-        sum(count) over (order by d asc rows between unbounded preceding and current row) as "tx_count"
-        from num_txs_per_day
+        f"""
+        select *
+        from {DBT_SCHEMA}.cum_txs_per_day
         """
     )
 
@@ -29,9 +22,7 @@ async def get_cumulative_txs():
 async def daily_txs():
     return await database.fetch_all(
         """
-        select date_trunc('day', timestamp) as "day", count(1) as "tx_count"
-        from txs
-        group by "day"
-        order by "day"
+        select *
+        from {DBT_SCHEMA}.num_txs_per_day
         """
     )
