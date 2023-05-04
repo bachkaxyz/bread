b8cbf24028117492dee57baa1a306a384bbbdbcc
Ethan Leifer <ethanleifer@gmail.com>
2023-05-04 14:53:26 -0400

    dbt ibc updates

---
+++ packages/dags/dbt_project/models/ibc/ibc_transfers.sql
@@ -0,0 +1,10 @@
+with
+    combined_transfers as (
+        select *, transfer_amount as amount_over_direction
+        from {{ ref("ibc_transfers_in") }}
+        union
+        select *, (transfer_amount::bigint * -1) as amount_over_direction
+        from {{ ref("ibc_transfers_out") }}
+    )
+select *
+from combined_transfers
---
+++ packages/dags/dbt_project/models/ibc/ibc_transfers_in.sql
@@ -0,0 +1,42 @@
+with
+    ibc_transfers_in as (
+        select
+            logs.txhash,
+            logs.msg_index,
+            txs.timestamp,
+            (
+                regexp_matches(
+                    jsonb_array_elements_text(parsed -> 'transfer_amount'), '[0-9]*'
+                )
+            )[1]::bigint as transfer_amount,
+            (
+                regexp_matches(
+                    jsonb_array_elements_text(parsed -> 'transfer_amount'), '\D+'
+                )
+            )[1] as transfer_denom,
+            jsonb_array_elements_text(
+                parsed -> 'fungible_token_packet_sender'
+            ) as ibc_sender,
+            jsonb_array_elements_text(
+                parsed -> 'fungible_token_packet_receiver'
+            ) as ibc_recv,
+            jsonb_array_elements_text(
+                parsed -> 'recv_packet_packet_src_port'
+            ) as src_port,
+            jsonb_array_elements_text(
+                parsed -> 'recv_packet_packet_src_channel'
+            ) as src_channel,
+            jsonb_array_elements_text(
+                parsed -> 'recv_packet_packet_dst_port'
+            ) as dst_port,
+            jsonb_array_elements_text(
+                parsed -> 'recv_packet_packet_dst_channel'
+            ) as dst_channel
+        from {{ source("indexer", "logs") }}
+        left join {{ source("indexer", "txs") }} on logs.txhash = txs.txhash
+        where
+            array(select jsonb_array_elements_text(logs.parsed -> 'message_module'))
+            = array['ibc_channel', 'ibc_channel']
+    )
+select *
+from ibc_transfers_in
