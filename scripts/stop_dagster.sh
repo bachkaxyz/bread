0711b914e8f4ec3b03f0ece2b5dd7fc73f8b9efe
Ethan Leifer <ethanleifer@gmail.com>
2023-05-05 15:08:56 -0400

    add more scripts

--- scripts/stop_dagster.sh
+++ scripts/stop_dagster.sh
@@ -9,13 +9,11 @@ if [ $ENVIRONMENT == ""]; then
   exit 0;
 elif [ $ENVIRONMENT == "production" ]; then
     echo "production mode";
-    DOCKER_BUILDKIT=0 docker compose -f docker-compose.local.yaml build;
-    docker compose -f docker-compose.prod.yaml -p ${COMPOSE_PREFIX} up -d;
+    docker compose -f docker-compose.prod.yaml -p ${COMPOSE_PREFIX} down;
     exit 0;
 elif [ $ENVIRONMENT == "development" ]; then
     echo "development mode";
-    DOCKER_BUILDKIT=0 docker compose -f docker-compose.local.yaml build;
-    docker compose -f docker-compose.local.yaml up -d;
+    docker compose -f docker-compose.local.yaml down;
 else
   echo "please set ENVIRONMENT to either 'production' or 'development' in root .env";
   exit 0;
