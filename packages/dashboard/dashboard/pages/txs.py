598fcf9ced50082121aac0155141203d8082fe7f
Ethan Leifer <ethanleifer@gmail.com>
2023-05-09 15:10:04 -0400

    api / dashboard and more models

--- packages/dashboard/dashboard/pages/txs.py
+++ packages/dashboard/dashboard/pages/txs.py
@@ -1,3 +1,4 @@
+import json
 import dash
 from dash import html, dcc, Input, Output, callback
 from dashboard.index import API_URL
@@ -6,14 +7,26 @@ import pandas as pd
 import plotly.express as px, plotly.graph_objects as go
 
 dash.register_page(__name__, path="/")
-print(f"{API_URL}/txs/daily")
-daily = requests.get(f"{API_URL}/txs/daily").json()
 
+daily = requests.get(f"{API_URL}/txs/daily").json()
 daily_df = pd.DataFrame(daily)
 
 cum = requests.get(f"{API_URL}/txs/cumulative").json()
-
 cum_df = pd.DataFrame(cum)
+
+network_providers = requests.get(f"{API_URL}/storage/providers/network").json()
+network_providers_df = pd.DataFrame(network_providers)
+network_providers_df.set_index("timestamp", inplace=True)
+
+prov_data = requests.get(f"{API_URL}/storage/providers").json()
+prov_df = pd.DataFrame(prov_data)
+
+used_space_df = pd.DataFrame.from_records(
+    prov_df["usedspace"].apply(json.loads), index=prov_df["timestamp"]
+)
+
+prov_count = requests.get(f"{API_URL}/storage/providers/count").json()
+prov_count_df = pd.DataFrame(prov_count, columns=["count", "timestamp"])
 
 layout = html.Div(
     children=[
@@ -28,5 +41,8 @@ layout = html.Div(
                 title="Cumulative Transactions",
             )
         ),
+        dcc.Graph(figure=px.line(network_providers_df, title="Network Space")),
+        dcc.Graph(figure=px.line(used_space_df, title="Used Space")),
+        dcc.Graph(figure=px.line(prov_count_df, title="Provider Count")),
     ]
 )
