d12955506a4b5c21fdcce6eca0d0b46c316fde9d
Ethan Leifer <ethanleifer@gmail.com>
2023-05-09 13:55:42 -0400

    add used_space and total space

--- packages/dags/dbt_project/models/jackal_providers/provider_space.sql
+++ packages/dags/dbt_project/models/jackal_providers/provider_space.sql
@@ -1,4 +1,4 @@
-select timestamp, json_object_agg(address, freespace)
+select timestamp, json_object_agg(address, freespace) as freespace, json_object_agg(address, used_space) as usedspace, json_object_agg(address, totalspace) as totalspace
 from {{ source('jackal_providers', 'providers')}}
 group by timestamp
 
