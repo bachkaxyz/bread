07f446fd184c3214c8eaac50d65a9f728bb567a4
Ethan Leifer <ethanleifer@gmail.com>
2023-05-09 15:09:48 -0400

    database quoting and script

--- packages/api/api/database.py
+++ packages/api/api/database.py
@@ -12,12 +12,11 @@ user = os.getenv("POSTGRES_USER")
 password = quote_plus(os.getenv("POSTGRES_PASSWORD", ""))
 database = os.getenv("POSTGRES_DB")
 
-DBT_SCHEMA = os.getenv("DBT_SCHEMA", "public")
-INDEXER_SCHEMA = os.getenv("INDEXER_SCHEMA", "public")
-DAGSTER_SCHEMA = os.getenv("DAGSTER_SCHEMA", "public")
+DBT_SCHEMA = f'"{os.getenv("DBT_SCHEMA", "public")}"'
+INDEXER_SCHEMA = f'"{os.getenv("INDEXER_SCHEMA", "public")}"'
+DAGSTER_SCHEMA = f'"{os.getenv("DAGSTER_SCHEMA", "public")}"'
 
 database_url = f"postgresql://{user}:{password}@{host}:{port}/{database}"
-
 if database_url is None:
     raise Exception("POSTGRES_URI environment variable not set")
 database = databases.Database(database_url)
