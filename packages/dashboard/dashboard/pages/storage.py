89c307cf277e3bc5ad9395086e71cbec49ed78b2
Ethan Leifer <ethanleifer@gmail.com>
2023-05-10 11:53:37 -0400

    remove by provider

--- packages/dashboard/dashboard/pages/storage.py
+++ packages/dashboard/dashboard/pages/storage.py
@@ -12,12 +12,12 @@ network_providers = requests.get(f"{API_URL}/storage/providers/network").json()
 network_providers_df = pd.DataFrame(network_providers)
 network_providers_df.set_index("timestamp", inplace=True)
 
-prov_data = requests.get(f"{API_URL}/storage/providers").json()
-prov_df = pd.DataFrame(prov_data)
+# prov_data = requests.get(f"{API_URL}/storage/providers").json()
+# prov_df = pd.DataFrame(prov_data)
 
-used_space_df = pd.DataFrame.from_records(
-    prov_df["usedspace"].apply(json.loads), index=prov_df["timestamp"]
-)
+# used_space_df = pd.DataFrame.from_records(
+#     prov_df["usedspace"].apply(json.loads), index=prov_df["timestamp"]
+# )
 
 prov_count = requests.get(f"{API_URL}/storage/providers/count").json()
 prov_count_df = pd.DataFrame(prov_count)
@@ -25,7 +25,7 @@ prov_count_df = pd.DataFrame(prov_count)
 layout = html.Div(
     children=[
         dcc.Graph(figure=px.line(network_providers_df, title="Network Space")),
-        dcc.Graph(figure=px.line(used_space_df, title="Used Space By Provider")),
+        # dcc.Graph(figure=px.line(used_space_df, title="Used Space By Provider")),
         dcc.Graph(
             figure=px.line(
                 prov_count_df, y="count", x="timestamp", title="Provider Count"
