8e2c6e69c42ec32a471690a327edf39a280ec748
Ethan Leifer <ethanleifer@gmail.com>
2023-05-11 14:54:54 -0400

    remove jackal dbt models from main

--- packages/dags/dbt_project/models/jackal_providers/provider_space.sql
+++
@@ -1,7 +0,0 @@
-select
-    timestamp,
-    json_object_agg(address, freespace) as freespace,
-    json_object_agg(address, totalspace - freespace) as usedspace,
-    json_object_agg(address, totalspace) as totalspace
-from {{ source("jackal_providers", "providers") }}
-group by timestamp
