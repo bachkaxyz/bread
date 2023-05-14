1b725462a32da36c5a8f1277ac1fcd5ee1ae1c2a
Ethan Leifer <ethanleifer@gmail.com>
2023-05-14 10:02:18 -0400

    ibc dash

--- packages/dashboard/dashboard/pages/ibc.py
+++ packages/dashboard/dashboard/pages/ibc.py
@@ -8,23 +8,27 @@ import plotly.express as px, plotly.graph_objects as go
 dash.register_page(__name__, path="/ibc", name="IBC")
 
 daily_cum = requests.get(f"{API_URL}/ibc/transfers/cumulative/daily").json()
-daily_cum_df = pd.DataFrame(daily_cum)
+daily_volume = requests.get(f"{API_URL}/ibc/transfers/volume/daily").json()
 
 layout = html.Div(
     children=[
         html.Div(
             children=[
-                html.Div(
-                    children=[
-                        html.Button("Daily", id="ibc_cum_daily"),
-                        html.Button("Hourly", id="ibc_cum_hourly"),
-                    ]
+                dcc.Graph(
+                    figure=px.line(
+                        daily_cum,
+                        x="day",
+                        y="cum_amount_over_direction",
+                        title="Cumulative IBC Transfers",
+                        color="transfer_denom",
+                    ),
+                    id="ibc_cum",
                 ),
                 dcc.Graph(
                     figure=px.line(
-                        daily_cum_df,
+                        daily_volume,
                         x="day",
-                        y="cum_amount_over_direction",
+                        y="total_amount_over_direction",
                         title="Cumulative IBC Transfers",
                         color="transfer_denom",
                     ),
