13fc022d658ce82fd9d1f91bf7ee41e7468a0abb
Ethan Leifer <ethanleifer@gmail.com>
2023-05-20 15:31:37 -0400

    update workflow and cli

--- packages/cli/cli/main.py
+++ packages/cli/cli/main.py
@@ -1,9 +1,63 @@
+import subprocess
 import typer
 
+app = typer.Typer()
 
-def main(name: str):
-    print(f"Hello {name}")
+test = typer.Typer()
 
+
+@test.command()
+def indexer():
+    remove_all_package_builds()
+    build_all_packages()
+    load_env_vars()
+    run_parse_tests()
+    run_indexer_tests()
+
+
+@test.command()
+def parse():
+    run_parse_tests()
+
+
+def remove_all_package_builds():
+    pass
+
+
+def load_env_vars():
+    subprocess.run("bash scripts/load_env_vars.sh", shell=True)
+
+
+def build_all_packages():
+    print("Building all packages...")
+    subprocess.run("bash poetry/build.sh", shell=True)
+
+
+def run_parse_tests():
+    print("Running parser tests...")
+    subprocess.run(
+        "docker compose -f packages/parse/docker-compose.tests.yaml down --remove-orphans",
+        shell=True,
+    )
+    subprocess.run(
+        "docker compose -f packages/parse/docker-compose.tests.yaml up --build",
+        shell=True,
+    )
+
+
+def run_indexer_tests():
+    print("Running indexer tests...")
+    subprocess.run(
+        "docker compose -f packages/indexer/docker-compose.tests.yaml down --remove-orphans",
+        shell=True,
+    )
+    subprocess.run(
+        "docker compose -f packages/indexer/docker-compose.tests.yaml up --build --abort-on-container-exit",
+        shell=True,
+    )
+
+
+app.add_typer(test, name="test")
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
