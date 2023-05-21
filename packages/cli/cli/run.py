16019fe18fd6c62f4c7e81880620dde9de5e83f5
Ethan Leifer <ethanleifer@gmail.com>
2023-05-20 23:37:06 -0400

    fix dashboard

--- packages/cli/cli/run.py
+++ packages/cli/cli/run.py
@@ -11,18 +11,73 @@ app = typer.Typer()
 @app.command()
 def indexer(redeploy: bool = True, prod: bool = False):
     compose_files: List[ValidPath] = ["packages/indexer/docker-compose.yaml"]
+    env = root_env_vars()
     if not prod:
         compose_files.append("packages/indexer/docker-compose.local.yaml")
-    docker = DockerClient(compose_files=compose_files, compose_env_file=".env")
+    docker = DockerClient(
+        compose_files=compose_files,
+        compose_env_file=".env",
+        compose_project_name=env["COMPOSE_PREFIX"] if env["COMPOSE_PREFIX"] else "",
+    )
 
     if redeploy:
         docker.compose.down(remove_orphans=True)
         remove_all_package_builds()
     build_all_packages()
 
-    docker.compose.up(detach=True, build=True)
+    try:
+        docker.compose.up(detach=True, build=True)
+    except:
+        raise typer.Exit(code=1)
 
 
 @app.command()
 def dagster(redeploy: bool = True, prod: bool = False):
-    compose_files: List[ValidPath] = ["packages/dagster/docker-compose.yaml"]
+    compose_files: List[ValidPath] = []
+    if prod:
+        compose_files.append("packages/dagster/docker-compose.prod.yaml")
+    else:
+        compose_files.append("packages/dagster/docker-compose.local.yaml")
+
+    env = root_env_vars()
+    docker = DockerClient(
+        compose_files=compose_files,
+        compose_env_file=".env",
+        compose_project_name=env["COMPOSE_PREFIX"] if env["COMPOSE_PREFIX"] else "",
+    )
+
+    if redeploy:
+        docker.compose.down(remove_orphans=True)
+        remove_all_package_builds()
+    build_all_packages()
+    docker.compose.up(detach=True, build=True)
+
+
+@app.command()
+def api():
+    compose_files: List[ValidPath] = ["packages/api/docker-compose.yaml"]
+    env = root_env_vars()
+    docker = DockerClient(
+        compose_files=compose_files,
+        compose_env_file=".env",
+        compose_project_name=env["COMPOSE_PREFIX"] if env["COMPOSE_PREFIX"] else "",
+    )
+    docker.compose.down(remove_orphans=True)
+    remove_all_package_builds()
+    build_all_packages()
+    docker.compose.up(detach=True, build=True)
+
+
+@app.command()
+def dashbord():
+    compose_files: List[ValidPath] = ["packages/dashboard/docker-compose.yaml"]
+    env = root_env_vars()
+    docker = DockerClient(
+        compose_files=compose_files,
+        compose_env_file=".env",
+        compose_project_name=env["COMPOSE_PREFIX"] if env["COMPOSE_PREFIX"] else "",
+    )
+    docker.compose.down(remove_orphans=True)
+    remove_all_package_builds()
+    build_all_packages()
+    docker.compose.up(detach=True, build=True)
